15b93dddfea1193417cc3c3006cc2ef370b7d93e
LivionLoran <66180915+LivionLoran@users.noreply.github.com>
2020-06-10 02:16:56 +0300

    Laba_7
    Link - form. Script - sending.

---
+++ Link.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Почтовый агент)</title>
+</head>
+<body>
+<form  method="POST" action="script.php">
+    <input type="text" name="Sub" size="43" value="Тема"><br>
+    <textarea name="Mes" cols="40" rows="10"></textarea><br>
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html
